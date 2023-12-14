da421623a79ef59e2c2be8e5ed817b1ab8522b6b
Ranuga <79456372+Programmer-RD-AI@users.noreply.github.com>
2023-12-15 01:19:22 +0530

    Create 27_switch.js

---
+++ modern_js/other_versions/27_switch.js
@@ -0,0 +1,21 @@
+const grade = "D";
+switch (grade) {
+  case "A":
+    console.log("u got an A");
+    break;
+  case "B":
+    console.log("u got a B");
+    break;
+  case "C":
+    console.log("u got a C");
+    break;
+  case "D":
+    console.log("u got a D");
+    break;
+  case "E":
+    console.log("u got a E");
+    break;
+  default:
+    console.log("non valid grade ekak bn..");
+    break;
+}
