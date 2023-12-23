ab21c954c86df7a9d6d99554c905d64ee5b6f414
Ranuga <79456372+Programmer-RD-AI@users.noreply.github.com>
2023-12-23 22:31:26 +0530

    Update sandbox.js

--- modern_js/sandbox.js
+++ modern_js/sandbox.js
@@ -1,5 +1,7 @@
 // const ul = document.querySelector("ul");
 // ul.remove();
+const button = document.querySelector("button");
+button.addEventListener("click", () => {});
 
 const items = document.querySelectorAll("li");
 items.forEach((element) => {
