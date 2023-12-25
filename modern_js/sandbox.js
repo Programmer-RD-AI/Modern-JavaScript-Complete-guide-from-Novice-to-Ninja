3f6e1c8137990af705f28282cfa6ec287e8cf026
Ranuga <79456372+Programmer-RD-AI@users.noreply.github.com>
2023-12-25 10:13:03 +0530

    Update sandbox.js

--- modern_js/sandbox.js
+++ modern_js/sandbox.js
@@ -1,1 +1,6 @@
-const copy = document.querySelector('')
+const copy = document.querySelector(".copy-me");
+copy.addEventListener("copy", () => {
+  console.log("u copied me !!");
+  //   copy.innerHTML =
+  //     "<img src='https://static-00.iconduck.com/assets.00/middle-finger-black-emoji-350x512-61ls9nsv.png'/>";
+});
