d073141a426be1fa81124bcd8a68cee0d4113d28
Ranuga <79456372+Programmer-RD-AI@users.noreply.github.com>
2023-12-25 23:24:40 +0530

    Update sandbox.js

---
+++ modern_js/sandbox.js
@@ -0,0 +1,5 @@
+const form = document.querySelector(".signup-form");
+form.addEventListener("submit", (e) => {
+  e.preventDefault(); // prevents the default action of a event (in this is to refresh the page)
+
+});
