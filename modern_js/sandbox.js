2bc28deed825d127e73f2bd24a518ba45821f193
Ranuga <79456372+Programmer-RD-AI@users.noreply.github.com>
2023-12-26 08:38:23 +0530

    Update sandbox.js

--- modern_js/sandbox.js
+++ modern_js/sandbox.js
@@ -1,5 +1,10 @@
 const form = document.querySelector(".signup-form");
+
 form.addEventListener("submit", (e) => {
   e.preventDefault(); // prevents the default action of a event (in this is to refresh the page)
-
+  const username = form.username.value;
+  const usernamepattern = /^[a-zA-Z]{6,12}$/;
+  if (usernamepattern.test(username)) {
+  } else {
+  }
 });
