8dbf42f94c6cae8bd5815a551816f09b2843e7fd
Ranuga <79456372+Programmer-RD-AI@users.noreply.github.com>
2023-12-27 18:33:38 +0530

    todo

--- todo/sandbox.js
+++ todo/sandbox.js
@@ -1,2 +1,46 @@
-const add = document.querySelector(".add");
-add.addEventListener("click", (event) => {});
+const addForm = document.querySelector(".add");
+const list = document.querySelector("ul");
+const search = document.querySelector("input.ss");
+const generate_template = (todo) => {
+  const html = `<li class="list-group-item d-flex justify-content-between align-items-center">
+            <span>${todo}</span>
+            <i class="far fa-trash-alt delete"></i>
+        </li>`;
+  list.innerHTML += html;
+
+  return html;
+};
+addForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const todo = addForm.add.value.trim(); // .trim() removes any blank space from the start and end
+  if (todo.length) {
+    //  > 0
+    generate_template(todo);
+    addForm.reset(); // reset all the input fields in an form
+  } else {
+    alert("There must be a value entered..!");
+  }
+});
+// delete todos
+list.addEventListener("click", (e) => {
+  if (e.target.classList.contains("delete")) {
+    e.target.parentElement.remove();
+  }
+});
+
+// keyup event
+
+const filterTodos = (term) => {
+  Array.from(list.children)
+    .filter((val) => {
+      return !val.textContent.includes(term);
+    })
+    .forEach((todo) => {
+      todo.classList.add("filtered");
+    });
+};
+search.addEventListener("keyup", (e) => {
+  e.name;
+  const term = search.value.trim();
+  filterTodos(term);
+});
