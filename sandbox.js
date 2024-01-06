6be328866fbf2adba861fc4a55141f1e7b5d69da
Ranuga <79456372+Programmer-RD-AI@users.noreply.github.com>
2024-01-07 04:35:45 +0530

    Update sandbox.js

--- sandbox.js
+++ sandbox.js
@@ -1,11 +1,22 @@
-// https://date-fns.org/
-
-const now = new Date();
-// console.log(now, dateFns.isToday(now));
-
-// formatting options
-console.log(dateFns.format(now, "YYYY"));
-console.log(dateFns.format(now, "MMM"));
-console.log(dateFns.format(now, "dddd"));
-console.log(dateFns.format(now, "Do"));
-console.log(dateFns.format(now, "dddd, Do, MMM, YYYY"));
+const getTodos = (callback) => {
+  const request = new XMLHttpRequest();
+  request.addEventListener("readystatechange", (e) => {
+    if (request.readyState === 4 && request.status === 200) {
+      callback(undefined, request.responseText);
+    } else if (request.readyState === 4) {
+      callback("could not fetch data", undefined);
+    }
+  });
+  request.open("GET", "https://jsonplaceholder.typicode.com/todos/");
+  request.send();
+};
+getTodos((err, data) => {
+  // error first always
+  if (err) {
+    console.log("OHHH NOOO");
+    console.log(err);
+  } else if (data) {
+    console.log("GOT THE DATA");
+    console.log(data);
+  }
+});
