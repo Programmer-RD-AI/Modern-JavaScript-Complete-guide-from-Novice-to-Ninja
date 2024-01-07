80f5074289a1ae6bb108f10c8cbc85701fa9a4fd
Ranuga <79456372+Programmer-RD-AI@users.noreply.github.com>
2024-01-08 00:13:22 +0530

    updates

--- sandbox.js
+++ sandbox.js
@@ -1,29 +1,24 @@
 const getTodos = (callback, location) => {
-  const request = new XMLHttpRequest();
-  request.addEventListener("readystatechange", () => {
-    if (request.readyState === 4 && request.status === 200) {
-      const data = JSON.parse(request.responseText);
-      callback(undefined, data);
-    } else if (request.readyState === 4) {
-      callback("could not fetch data", undefined);
-    }
+  return new Promise((resolve, reject) => {
+    const request = new XMLHttpRequest();
+    request.addEventListener("readystatechange", () => {
+      if (request.readyState === 4 && request.status === 200) {
+        const data = JSON.parse(request.responseText);
+        resolve(data);
+      } else if (request.readyState === 4) {
+        reject("couldnt not get some data");
+      }
+    });
+    request.open("GET", location);
+    request.send();
   });
-  // request.open("GET", "https://jsonplaceholder.typicode.com/todos/");
-  request.open("GET", location);
-  request.send();
 };
-console.log(1);
-console.log(2);
 
-getTodos((err, data) => {
-  console.log(data, err);
-  getTodos((err, data) => {
-    console.log(data, err);
-    getTodos((err, data) => {
-      console.log(data, err);
-    },"./todos/shaun.json");
-  }, "./todos/mario.json");
-}, "./todos/lugii.json");
-
-console.log(3);
-console.log(4);
+getTodos("./todos/lugii.json")
+  .then((data) => {
+    console.log("promise resolved: ", data);
+    // getTodos("./todos/mario.json");
+  })
+  .catch((err) => {
+    console.log("promise rejected: ", err);
+  });
