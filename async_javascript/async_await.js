9c39cce1794c06ea3cfc695eb988acccc6bbc657
Ranuga <79456372+Programmer-RD-AI@users.noreply.github.com>
2024-01-09 22:50:37 +0530

    Update async_await.js

--- async_javascript/async_await.js
+++ async_javascript/async_await.js
@@ -1,17 +1,17 @@
 const getTodos = async () => {
   const response = await fetch("./todos/lugii.json");
+  console.log(response);
+  if (response.status !== 200) {
+    throw new Error("gg");
+  }
   const data = await response.json();
   return data;
 };
 
-console.log(1);
-console.log(2);
 getTodos()
   .then((data) => {
     console.log(data);
   })
   .catch((err) => {
-    console.log(err);
+    console.log(err.message);
   });
-console.log(3);
-console.log(4);
