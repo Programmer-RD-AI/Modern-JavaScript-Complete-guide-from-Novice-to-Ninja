039f70949f6bf2b3e4129b8041ab9411e6c8e5d6
Ranuga <79456372+Programmer-RD-AI@users.noreply.github.com>
2024-01-09 10:20:53 +0530

    Update sandbox.js

--- sandbox.js
+++ sandbox.js
@@ -1,1 +1,17 @@
-// fetch api
+const getTodos = async () => {
+  const response = await fetch("./todos/lugii.json");
+  console.log(response);
+  if (response.status !== 200) {
+    throw new Error("gg");
+  }
+  const data = await response.json();
+  return data;
+};
+
+getTodos()
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
