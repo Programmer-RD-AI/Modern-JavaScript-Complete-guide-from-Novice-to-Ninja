af0a6a72b353d6ffec1c326eb2d5b18c5185681c
Ranuga <79456372+Programmer-RD-AI@users.noreply.github.com>
2024-01-09 22:50:56 +0530

    Create forecast.js

---
+++ weather_ninja/scripts/forecast.js
@@ -0,0 +1,25 @@
+const key = "pS8euTRb9TEA8LwTUoRDPVhUsqvENABz";
+const getCity = async (cityName) => {
+  const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
+  const query = `?apikey=${key}&q=${cityName}`;
+  const response = await fetch(base + query);
+  const data = await response.json();
+  return data[0];
+};
+const getConditions = async (cityCode) => {
+  const base = `http://dataservice.accuweather.com/currentconditions/v1/`;
+  const query = `${cityCode}?apikey=${key}`;
+  const response = await fetch(base + query);
+  const data = await response.json();
+  return data;
+};
+getCity("colombo")
+  .then((data) => {
+    return getConditions(data["Key"]);
+  })
+  .then((conditions) => {
+    console.log(conditions[0]["Temperature"]["Metric"]);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
