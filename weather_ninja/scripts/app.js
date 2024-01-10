fd9fd412e54005fb12f2a7d1a5217bc217b39436
Ranuga <79456372+Programmer-RD-AI@users.noreply.github.com>
2024-01-11 01:15:35 +0530

    Update app.js

--- weather_ninja/scripts/app.js
+++ weather_ninja/scripts/app.js
@@ -1,4 +1,18 @@
+const card = document.querySelector(".card");
+const details = document.querySelector(".details");
 const location_form = document.querySelector("form");
+const updateUI = (data) => {
+  const cityDets = data.cityDets;
+  const cityWeather = data.cityWeather;
+  details.innerHTML = `
+  <h5 class="my-3">${cityDets['EnglishName']}</h5>
+          <div class="my-3">Weather Condition</div>
+          <div class="display-4 my-4">
+            <span>temp</span>
+            <span>&deg;C</span>
+          </div>`;
+};
+
 const updateCity = async (city) => {
   const cityDets = await getCity(city);
   const cityWeather = await getConditions(cityDets["Key"]);
@@ -14,7 +28,7 @@ location_form.addEventListener("submit", (e) => {
   // update the ui with the new city
   updateCity(city)
     .then((data) => {
-      console.log(data);
+      updateUI(data);
     })
     .catch((error) => {
       alert(error);
