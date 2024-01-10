9ecc0b921bda71370d547ac62879f34dbbae494f
Ranuga <79456372+Programmer-RD-AI@users.noreply.github.com>
2024-01-10 14:53:07 +0530

    Update app.js

---
+++ weather_ninja/scripts/app.js
@@ -0,0 +1,22 @@
+const location_form = document.querySelector("form");
+const updateCity = async (city) => {
+  const cityDets = await getCity(city);
+  const cityWeather = await getConditions(cityDets["Key"]);
+  return {
+    cityDets: cityDets,
+    cityWeather: cityWeather,
+  };
+};
+location_form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const city = location_form.city.value.trim();
+  location_form.reset();
+  // update the ui with the new city
+  updateCity(city)
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((error) => {
+      alert(error);
+    });
+});
