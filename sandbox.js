d09c144fc9584d5c580b50afa4f0d3801bc0ae5b
Ranuga <79456372+Programmer-RD-AI@users.noreply.github.com>
2024-01-12 17:26:10 +0530

    Update sandbox.js

---
+++ sandbox.js
@@ -0,0 +1,9 @@
+// store data
+localStorage.setItem("name", "hrterhrter");
+localStorage.setItem("age", "50");
+// get data
+let name = localStorage.getItem("name");
+let age = localStorage.getItem("age");
+console.log(name, age);
+// update data
+localStorage.removeItem("age");
