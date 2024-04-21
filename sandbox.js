ab2d2a2e1705576d3d0ed276e711a7a628efa877
Ranuga <79456372+Programmer-RD-AI@users.noreply.github.com>
2024-04-21 16:52:31 +0530

    Update sandbox.js

--- sandbox.js
+++ sandbox.js
@@ -1,12 +1,16 @@
 class User {
-  constructor(name, email) {
+  constructor(username, email) {
     // set up properties in an object
-    this.name = name;
+    this.username = username;
     this.email = email;
   }
   login() {
-    
+    console.log(`${this.username} just logged in`);
   } // arrow functions dont use this object instance.
+
+  logout() {
+    console.log(`${this.username} logged out`);
+  }
 }
 
 const userOne = new User("mario", "mario@gmail.com");
